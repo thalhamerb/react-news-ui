4d9f8a651871345deef9d9a775c43b85852dbde6
Brian <thalhamerb@gmail.com>
2020-08-03 16:11:50 -0400

    finished sign up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom
 import About from "./header/About";
 import Header from "./header/Header";
 import Cookies from 'universal-cookie';
+import Signup from "./header/SignUp";
 
 function App() {
     const cookies = new Cookies();
@@ -18,6 +19,7 @@ function App() {
                 <Redirect from='/' exact to='/section/general' />
                 <Route path="/section/:section" render={(props) => <Stories key={props.match.params.section} {...props} />} />
                 <Route path="/about" component={About} />
+                <Route path={"/sign-up"}  component={Signup} />
             </Switch>
         </Router>
     </div>
--- src/header/SignUp.js
+++ src/header/SignUp.js
@@ -4,41 +4,76 @@ import * as Yup from 'yup';
 
 function Signup() {
     return (
-        <Formik
-            initialValues={{ firstName: '', lastName: '', email: '' }}
-            validationSchema={Yup.object({
-                firstName: Yup.string()
-                    .max(15, 'Must be 15 characters or less')
-                    .required('Required'),
-                lastName: Yup.string()
-                    .max(20, 'Must be 20 characters or less')
-                    .required('Required'),
-                email: Yup.string()
-                    .email('Invalid email address')
-                    .required('Required'),
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}
-        >
-            <Form>
-                <label htmlFor="firstName">First Name</label>
-                <Field name="firstName" type="text" />
-                <ErrorMessage name="firstName" />
-                <label htmlFor="lastName">Last Name</label>
-                <Field name="lastName" type="text" />
-                <ErrorMessage name="lastName" />
-                <label htmlFor="email">Email Address</label>
-                <Field name="email" type="email" />
-                <ErrorMessage name="email" />
-                <button type="submit">Submit</button>
-            </Form>
-        </Formik>
+        <>
+            <h3>
+                Let's get you info...
+            </h3>
+            <Formik
+                initialValues={{firstName: '', lastName: '', email: ''}}
+                validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .trim()
+                        .matches(/^[A-Z,a-z]+$/, "Only letters allowed")
+                        .required('Required'),
+                    lastName: Yup.string()
+                        .trim()
+                        .matches(/^[A-Z,a-z]+$/, "Only letters allowed")
+                        .required('Required'),
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                    password: Yup.string()
+                        .min(8, 'Must be 8 characters long')
+                        .matches('(?=.*[A-Z])', 'Must contain at least one upper case letter')
+                        .required('Required'),
+                    age: Yup.number()
+                        .min(10, 'Must be at least 10 years old to join')
+                        .max(150, 'You cannot be immortal')
+                        .integer('No partial ages')
+                })}
+                onSubmit={(values, {setSubmitting}) => {
+                    const timeOut = setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+
+                        clearTimeout(timeOut)
+                    }, 100);
+                }}
+            >
+                <Form>
+                    <div>
+                        <label htmlFor="firstName">First Name</label>
+                        <Field name="firstName" type="text"/>
+                        <ErrorMessage name="firstName"/>
+                    </div>
+                    <div>
+                        <label htmlFor="lastName">Last Name</label>
+                        <Field name="lastName" type="text"/>
+                        <ErrorMessage name="lastName"/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <Field name="email" type="email"/>
+                        <ErrorMessage name="email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" type="text" placeholder="Password"/>
+                        <ErrorMessage name="password"/>
+                    </div>
+                    <div>
+                        <label htmlFor="age">Age</label>
+                        <Field name="age" type="text"/>
+                        <ErrorMessage name="age"/>
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </Form>
+            </Formik>
+        </>
     );
 };
 
 
-export default SignUp;
+export default Signup;
