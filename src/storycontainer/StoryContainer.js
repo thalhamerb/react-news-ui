fb2c9c736eb1814e10dfb7eea33b9d508a980766
thalhamerb <thalhamerb@gmail.com>
2020-08-18 22:51:35 -0400

    routing simplifications.  It's so beautiful now..

--- src/storycontainer/StoryContainer.js
+++ src/storycontainer/StoryContainer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import StoryBody from "./StoryBody"
 import {Route} from "react-router-dom";
 import * as routes from "../routes";
